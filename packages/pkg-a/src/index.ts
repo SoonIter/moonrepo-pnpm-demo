2880e03440422839776d3465ad491002b83f4641
SoonIter <sooniter@gmail.com>
2023-02-23 10:03:21 +0800

    chore: init

---
+++ packages/pkg-a/src/index.ts
@@ -0,0 +1,15 @@
+import { sum } from '@pnpm-moon-demo/pkg-b';
+
+const add = (...args: number[]) => args.reduce((prev, curr) => prev + curr, 0);
+export { add, sum };
+export default add;
+
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
---
+++ packages/pkg-b/src/index.ts
@@ -0,0 +1,2 @@
+const sum = (...args: number[]) => args.reduce((prev, curr) => prev + curr, 0);
+export { sum };
